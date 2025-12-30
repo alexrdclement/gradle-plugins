52c41b1a0611e6f0269d042d3919d8d43b9769ca
Alex Clement <alexrdclement@gmail.com>
2025-12-30 10:02:14 -0500

    Fix release plugin

--- plugins/src/main/kotlin/GithubReleaseConventionPlugin.kt
+++ plugins/src/main/kotlin/GithubReleaseConventionPlugin.kt
@@ -1,6 +1,8 @@
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.jetbrains.kotlin.gradle.plugin.extraProperties
+import org.shipkit.changelog.GenerateChangelogTask
+import org.shipkit.github.release.GithubReleaseTask
 
 open class GithubReleaseExtension {
     var githubToken: String? = null
@@ -16,24 +18,27 @@ class GithubReleaseConventionPlugin : Plugin<Project> {
         project.pluginManager.apply("org.shipkit.shipkit-github-release")
 
         val extension = project.extensions.create("githubRelease", GithubReleaseExtension::class.java)
-        project.afterEvaluate {
-            val changelogTask = project.tasks.findByName("generateChangelog")
-            val previousRevision = project.extraProperties["shipkit-auto-version.previous-tag"] as String
-            changelogTask?.let { task ->
-                task.setProperty("previousRevision", previousRevision)
-                extension.githubToken?.let { task.setProperty("githubToken", it) }
-                extension.repository?.let { task.setProperty("repository", it) }
-            } ?: return@afterEvaluate
 
-            val githubReleaseTask = project.tasks.findByName("githubRelease")
-            githubReleaseTask?.let { task ->
-                task.dependsOn(changelogTask)
-                task.setProperty("enabled", extension.enabled)
-                extension.repository?.let { task.setProperty("repository", it) }
-                // changelog property is set automatically by Shipkit
-                extension.githubToken?.let { task.setProperty("githubToken", it) }
-                extension.newTagRevision?.let { task.setProperty("newTagRevision", it) }
-            }
+        // Configure tasks after evaluation when extension values are available
+        project.afterEvaluate {
+            val previousRevision = project.extraProperties["shipkit-auto-version.previous-tag"] as String
+
+            // Configure generateChangelog task using typed API
+            val changelogTask = project.tasks.named("generateChangelog", GenerateChangelogTask::class.java).get()
+            changelogTask.previousRevision = previousRevision
+            extension.githubToken?.let { token -> changelogTask.githubToken = token }
+            extension.repository?.let { repo -> changelogTask.repository = repo }
+
+            // Configure githubRelease task using typed API
+            val githubReleaseTask = project.tasks.named("githubRelease", GithubReleaseTask::class.java).get()
+            githubReleaseTask.dependsOn(changelogTask)
+            githubReleaseTask.enabled = extension.enabled
+            extension.repository?.let { repo -> githubReleaseTask.repository = repo }
+            // Wire changelog output from generateChangelog to githubRelease
+            // Use setProperty for changelog as it may not have a typed accessor
+            githubReleaseTask.setProperty("changelog", changelogTask.outputFile)
+            extension.githubToken?.let { token -> githubReleaseTask.githubToken = token }
+            extension.newTagRevision?.let { revision -> githubReleaseTask.newTagRevision = revision }
         }
     }
 }
