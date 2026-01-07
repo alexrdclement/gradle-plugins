a5482e14aa4f55b56e07a9d368d23da119645dbf
Alex Clement <alexrdclement@gmail.com>
2026-01-07 09:59:28 -0500

    Support disabling targets via local.properties for faster sync times

--- plugins/src/main/kotlin/KotlinMultiplatformLibraryConventionPlugin.kt
+++ plugins/src/main/kotlin/KotlinMultiplatformLibraryConventionPlugin.kt
@@ -12,6 +12,47 @@ import org.gradle.kotlin.dsl.invoke
 import org.jetbrains.kotlin.gradle.ExperimentalWasmDsl
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
+enum class KotlinTarget {
+    ANDROID,
+    JVM,
+    IOS,
+    WASM,
+    ;
+}
+
+/**
+ * Configure a Kotlin multiplatform library project.
+ *
+ * Example usage:
+ * ```kotlin
+ * kotlin {
+ *     libraryTargets(
+ *         androidNamespace = "com.alexrdclement.library",
+ *         iosFrameworkBaseName = "MyLibrary",
+ *     )
+ * ...
+ * }
+ * ```
+ *
+ * Optionally configure enabled targets via `libraryTargets`:
+ * ```kotlin
+ * kotlin {
+ *     libraryTargets(
+ *         androidNamespace = "com.alexrdclement.library",
+ *         iosFrameworkBaseName = "MyLibrary",
+ *         targets = setOf(KotlinTarget.ANDROID, KotlinTarget.IOS),
+ *     )
+ * ...
+ * ```
+ *
+ * or local.properties:
+ * ```
+ * kotlin.target.android=true
+ * kotlin.target.ios=false
+ * ```
+ *
+ * Defaults to the full set of targets if not specified.
+ */
 class KotlinMultiplatformLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) = with(target) {
         with(pluginManager) {
@@ -26,7 +67,8 @@ class KotlinMultiplatformLibraryConventionPlugin : Plugin<Project> {
 fun Project.libraryTargets(
     androidNamespace: String,
     iosFrameworkBaseName: String,
-    iosFrameworkIsStatic: Boolean = true
+    iosFrameworkIsStatic: Boolean = true,
+    targets: Set<KotlinTarget>? = null
 ) {
     libraryTargets(
         androidConfiguration = AndroidLibraryTargetConfiguration(
@@ -37,39 +79,51 @@ fun Project.libraryTargets(
                 baseName = iosFrameworkBaseName,
                 isStatic = iosFrameworkIsStatic
             )
-        )
+        ),
+        targets = targets
     )
 }
 
 fun Project.libraryTargets(
     androidConfiguration: AndroidLibraryTargetConfiguration,
-    iosConfiguration: IosLibraryTargetConfiguration
+    iosConfiguration: IosLibraryTargetConfiguration,
+    targets: Set<KotlinTarget>? = null
 ) {
+    val enabledTargets = targets ?: loadTargetsFromProperties()
+
     extensions.configure<KotlinMultiplatformExtension> {
         applyDefaultHierarchyTemplate()
 
-        androidLibrary {
-            configureKotlinMultiplatformAndroidLibrary(this, androidConfiguration)
-        }
-
-        jvm()
-
-        val iosFrameworkConfiguration = iosConfiguration.framework
-        iosArm64 {
-            binaries.framework {
-                configure(iosFrameworkConfiguration)
-            }
-        }
-        iosSimulatorArm64 {
-            binaries.framework {
-                configure(iosFrameworkConfiguration)
+        if (KotlinTarget.ANDROID in enabledTargets) {
+            androidLibrary {
+                configureKotlinMultiplatformAndroidLibrary(this, androidConfiguration)
             }
         }
 
-        @OptIn(ExperimentalWasmDsl::class)
-        wasmJs {
-            browser()
-            binaries.executable()
+        if (KotlinTarget.JVM in enabledTargets) {
+            jvm()
+        }
+
+        if (KotlinTarget.IOS in enabledTargets) {
+            val iosFrameworkConfiguration = iosConfiguration.framework
+            iosArm64 {
+                binaries.framework {
+                    configure(iosFrameworkConfiguration)
+                }
+            }
+            iosSimulatorArm64 {
+                binaries.framework {
+                    configure(iosFrameworkConfiguration)
+                }
+            }
+        }
+
+        if (KotlinTarget.WASM in enabledTargets) {
+            @OptIn(ExperimentalWasmDsl::class)
+            wasmJs {
+                browser()
+                binaries.executable()
+            }
         }
 
         sourceSets {
@@ -79,5 +133,23 @@ fun Project.libraryTargets(
                 }
             }
         }
+    }
+}
+
+private fun Project.loadTargetsFromProperties(): Set<KotlinTarget> {
+    val localPropsFile = rootProject.file("local.properties")
+    if (!localPropsFile.exists()) {
+        return KotlinTarget.entries.toSet() // All targets enabled by default
+    }
+
+    val localProps = java.util.Properties()
+    localPropsFile.inputStream().use { localProps.load(it) }
+
+    // Start with all targets, remove explicitly disabled ones
+    return KotlinTarget.entries.toMutableSet().apply {
+        if (localProps.getProperty("kotlin.target.android") == "false") remove(KotlinTarget.ANDROID)
+        if (localProps.getProperty("kotlin.target.jvm") == "false") remove(KotlinTarget.JVM)
+        if (localProps.getProperty("kotlin.target.ios") == "false") remove(KotlinTarget.IOS)
+        if (localProps.getProperty("kotlin.target.wasm") == "false") remove(KotlinTarget.WASM)
     }
 }
