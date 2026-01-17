4d34a7991dbbffba2d045ee9bd4ea88c90886711
Alex Clement <alexrdclement@gmail.com>
2026-01-17 09:42:40 -0500

    Expose compose multiplatform versions via plugin extension

--- plugins/src/main/kotlin/ComposeMultiplatformConventionPlugin.kt
+++ plugins/src/main/kotlin/ComposeMultiplatformConventionPlugin.kt
@@ -2,8 +2,6 @@ import com.alexrdclement.gradle.plugin.Libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.kotlin.dsl.configure
-import org.gradle.kotlin.dsl.getByType
-import org.jetbrains.compose.ComposeExtension
 import org.jetbrains.kotlin.compose.compiler.gradle.ComposeCompilerGradlePluginExtension
 import org.jetbrains.kotlin.gradle.dsl.KotlinMultiplatformExtension
 
@@ -12,43 +10,44 @@ class ComposeMultiplatformConventionPlugin : Plugin<Project> {
         pluginManager.apply("org.jetbrains.compose")
         pluginManager.apply("org.jetbrains.kotlin.plugin.compose")
 
+        val composeMultiplatform = extensions.create("composeMultiplatform", ComposeMultiplatformExtension::class.java)
+
         afterEvaluate {
-            configureCompose()
+            configureCompose(composeMultiplatform)
         }
     }
 }
 
-fun Project.configureCompose() {
+fun Project.configureCompose(multiplatformExtension: ComposeMultiplatformExtension) {
     composeCompiler {
         includeSourceInformation.set(true)
     }
 
-    val compose = extensions.getByType<ComposeExtension>()
     extensions.configure<KotlinMultiplatformExtension> {
         sourceSets.apply {
             getByName("commonMain").dependencies {
-                implementation(compose.dependencies.components.uiToolingPreview)
-                implementation(compose.dependencies.runtime)
-                implementation(compose.dependencies.ui)
-                implementation(compose.dependencies.foundation)
+                implementation(multiplatformExtension.uiToolingPreview)
+                implementation(multiplatformExtension.runtime)
+                implementation(multiplatformExtension.ui)
+                implementation(multiplatformExtension.foundation)
             }
 
             findByName("androidMain")?.apply {
                 dependencies {
-                    implementation(compose.dependencies.preview)
+                    implementation(multiplatformExtension.preview)
 
                     // TODO: revisit in later versions of Otter. See https://issuetracker.google.com/issues/422373442
-                    implementation(compose.dependencies.uiTooling)
-                    implementation(Libs.androidxActivityCompose)
-                    implementation(Libs.androidxCustomview)
-                    implementation(Libs.androidxEmoji2)
-                    implementation(Libs.composeUiTestManifest)
+                    implementation(multiplatformExtension.uiTooling)
+                    implementation(ComposeMultiplatformExtension.Android.activityCompose)
+                    implementation(ComposeMultiplatformExtension.Android.customview)
+                    implementation(ComposeMultiplatformExtension.Android.emoji2)
+                    implementation(ComposeMultiplatformExtension.Android.uiTestManifest)
                 }
             }
 
             findByName("jvmMain")?.apply {
                 dependencies {
-                    implementation(compose.dependencies.desktop.currentOs)
+                    implementation(multiplatformExtension.desktopCurrentOs)
                 }
             }
         }
@@ -57,4 +56,23 @@ fun Project.configureCompose() {
 
 fun Project.composeCompiler(block: ComposeCompilerGradlePluginExtension.() -> Unit) {
     extensions.configure<ComposeCompilerGradlePluginExtension>(block)
+}
+
+open class ComposeMultiplatformExtension {
+    val ui: String = Libs.composeMultiplatformUi
+    val foundation: String = Libs.composeMultiplatformFoundation
+    val runtime: String = Libs.composeMultiplatformRuntime
+
+    val preview: String = Libs.composeMultiplatformUiToolingPreview
+    val uiTooling: String = Libs.composeMultiplatformUiTooling
+    val uiToolingPreview: String = Libs.composeMultiplatformUiToolingPreview
+
+    val desktopCurrentOs: String = Libs.composeMultiplatformDesktop
+
+    object Android {
+        val activityCompose: String = Libs.androidxActivityCompose
+        val customview: String = Libs.androidxCustomview
+        val emoji2: String = Libs.androidxEmoji2
+        val uiTestManifest: String = Libs.composeUiTestManifest
+    }
 }
